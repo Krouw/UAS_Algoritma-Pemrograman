2b7062093c89d4458239f015be6c8bfd00863fa9
Krouw <tekmon02@gmail.com>
2026-02-08 15:53:53 +0900

    Add files via upload

---
+++ UAS2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Function untuk menghitung potongan
+long potong(long belanja) {
+    double diskon = 0;
+
+    if (belanja < 1000000) {
+        diskon = 0;
+    } else if (belanja <= 5000000) {
+        diskon = 0.20;
+    } else {
+        diskon = 0.35;
+    }
+
+    return belanja * diskon;
+}
+
+int main() {
+    long belanja, potongan, bayar;
+    char ulang;
+
+    do {
+        cout << "Program Hitung Potongan.\n";
+        cout << "Besar pembelian barang            Rp. ";
+        cin >> belanja;
+
+        potongan = potong(belanja);
+        bayar = belanja - potongan;
+
+        cout << "Besar diskon yang diberikan       Rp. " << potongan << endl;
+        cout << "Besar harga yang harus dibayarkan Rp. " << bayar << endl;
+
+        cout << "\nIngin menjalankan program dari awal (y/n)? ";
+        cin >> ulang;
+        cout << endl;
+
+    } while (ulang == 'y' || ulang == 'Y');
+
+    return 0;
+}
---
+++ UAS3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function untuk menentukan tarif sewa
+int Tarif(char kode, string &jenisBuku) {
+    int tarif = 0;
+
+    if (kode == 'C' || kode == 'c') {
+        jenisBuku = "CerPen (Kumpulan Cerita Pendek)";
+        tarif = 500;
+    }
+    else if (kode == 'K' || kode == 'k') {
+        jenisBuku = "Komik";
+        tarif = 700;
+    }
+    else if (kode == 'N' || kode == 'n') {
+        jenisBuku = "Novel";
+        tarif = 1000;
+    }
+    else {
+        jenisBuku = "Tidak Diketahui";
+        tarif = 0;
+    }
+
+    return tarif;
+}
+
+int main() {
+    char ulang;
+    do {
+        string nama, jenisBuku;
+        char kode;
+        int banyakPinjam, tarifSewa, totalBayar;
+
+        cout << "Perpustakaan \"Kecil-Kecilan\"" << endl;
+        cout << "-----------------------------" << endl;
+
+        cout << "Nama Penyewa Buku     : ";
+        getline(cin, nama);
+
+        cout << "Kode Buku [C/K/N]     : ";
+        cin >> kode;
+
+        cout << "Banyak Pinjam         : ";
+        cin >> banyakPinjam;
+
+        tarifSewa = Tarif(kode, jenisBuku);
+        totalBayar = tarifSewa * banyakPinjam;
+
+        cout << endl;
+        cout << "Tarif Sewa            : Rp." << tarifSewa << endl;
+        cout << "Jenis Buku            : " << jenisBuku << endl;
+        cout << endl;
+        cout << "Penyewa dengan Nama " << nama << endl;
+        cout << "Jumlah Bayar Penyewaan Sebesar Rp." << totalBayar << endl;
+        cout << endl;
+
+        cout << "Ingin menjalankan program dari awal (y/n) ? ";
+        cin >> ulang;
+        cin.ignore();
+
+        cout << endl;
+    } while (ulang == 'y' || ulang == 'Y');
+
+    return 0;
+}
